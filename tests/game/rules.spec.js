b2a2ded01b5b532818bdcbecd5d7e82ac9b6ed4f
Ricardo Conceição <ricardo.conceicao@getnetalbatross.onmicrosoft.com>
2020-01-14 18:44:58 -0300

    -Implementing testes for player 2 as the winner

--- tests/game/rules.spec.js
+++ tests/game/rules.spec.js
@@ -1,5 +1,6 @@
-import { expect } from 'chai';
-import { checkWinner } from '../../src/game/rules';
+import {expect} from 'chai';
+import {checkWinner} from '../../src/game/rules';
+import {STONE, SCISOR, PAPER} from '../../src/game/game-items';
 
 describe('Rules', () => {
   context('Smoke Test', () => {
@@ -9,28 +10,97 @@ describe('Rules', () => {
   });
 
   context('Check winner', () => {
-    let player1;
-    let player2;
+    context('When `Player 1` is the winner', () => {
+      let player1;
+      let player2;
 
-    beforeEach(() => {
-      player1 = {
-        name: 'player1',
-        move: 'stone',
-      };
+      before(() => {
+        player1 = {
+          name: 'player1',
+          choice: STONE,
+        };
 
-      player2 = {
-        name: 'player2',
-        move: 'paper',
-      };
+        player2 = {
+          name: 'player2',
+          choice: SCISOR,
+        };
+      });
+
+      it('Should return `Player 1` as the winner choosing `STONE`', () => {
+        const result = checkWinner(player1, player2);
+        expect(result).to.not.be.equal(null);
+        expect(result).to.have.property('name');
+        expect(result).to.have.property('choice');
+        expect(result.name).to.be.equal(player1.name);
+        expect(result.choice).to.be.equal(player1.choice);
+      });
+
+      it('Should return `Player 1` as the winner choosing `PAPER`', () => {
+        player1.choice = PAPER;
+        player2.choice = STONE;
+        const result = checkWinner(player1, player2);
+        expect(result).to.not.be.equal(null);
+        expect(result).to.have.property('name');
+        expect(result).to.have.property('choice');
+        expect(result.name).to.be.equal(player1.name);
+        expect(result.choice).to.be.equal(player1.choice);
+      });
+
+      it('Should return `Player 1` as the winner choosing `SCISOR`', () => {
+        player1.choice = SCISOR;
+        player2.choice = PAPER
+        const result = checkWinner(player1, player2);
+        expect(result).to.not.be.equal(null);
+        expect(result).to.have.property('name');
+        expect(result).to.have.property('choice');
+        expect(result.name).to.be.equal(player1.name);
+        expect(result.choice).to.be.equal(player1.choice);
+      });
     });
+    context('When `Player 2` is the winner', () => {
+      let player1;
+      let player2;
 
-    it('Should return `player1` as the winner', () => {
-      const result = checkWinner(player1, player2);
-      expect(result).to.not.be.equal(null);
-      expect(result).to.have.property('name');
-      expect(result).to.have.property('move');
-      expect(result.name).to.be.equal(player1.name);
-      expect(result.move).to.be.equal(player1.move);
+      before(() => {
+        player1 = {
+          name: 'player1',
+          choice: SCISOR,
+        };
+
+        player2 = {
+          name: 'player2',
+          choice: STONE,
+        };
+      });
+
+      it('Should return `Player 2` as the winner choosing `STONE`', () => {
+        const result = checkWinner(player1, player2);
+        expect(result).to.not.be.equal(null);
+        expect(result).to.have.property('name');
+        expect(result).to.have.property('choice');
+        expect(result.name).to.be.equal(player2.name);
+        expect(result.choice).to.be.equal(player2.choice);
+      });
+
+      it('Should return `Player 2` as the winner choosing `PAPER`', () => {
+        player2.choice = PAPER;
+        const result = checkWinner(player1, player2);
+        expect(result).to.not.be.equal(null);
+        expect(result).to.have.property('name');
+        expect(result).to.have.property('choice');
+        expect(result.name).to.be.equal(player2.name);
+        expect(result.choice).to.be.equal(player2.choice);
+      });
+
+      it('Should return `Player 2` as the winner choosing `SCISOR`', () => {
+        player2.choice = SCISOR;
+        const result = checkWinner(player1, player2);
+        expect(result).to.not.be.equal(null);
+        expect(result).to.have.property('name');
+        expect(result).to.have.property('choice');
+        expect(result.name).to.be.equal(player2.name);
+        expect(result.choice).to.be.equal(player2.choice);
+      });
     });
   });
 });
